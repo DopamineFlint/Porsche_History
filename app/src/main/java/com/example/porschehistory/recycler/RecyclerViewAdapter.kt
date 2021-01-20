06559121082a379a40696c66c07902e96b66b52f
Flint Dopamine <dopamine.flint@gmail.com>
2021-01-20 12:13:32 +0300

    Made items in RecyclerView clickable

--- app/src/main/java/com/example/porschehistory/recycler/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/porschehistory/recycler/RecyclerViewAdapter.kt
@@ -9,7 +9,8 @@ import com.example.porschehistory.R
 import com.example.porschehistory.data.Year
 import kotlinx.android.synthetic.main.example_item.view.*
 
-class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder>() {
+class RecyclerViewAdapter(private val listener: OnItemClickListener) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ExampleViewHolder>() {
 
     private var yearList = emptyList<Year>()
 
@@ -32,8 +33,24 @@ class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.ExampleVi
 
     override fun getItemCount() = yearList.size //exampleList.size
 
-    class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ExampleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
         val textView: TextView = itemView.text_view_card_item
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
     }
 
     fun setData(year: List<Year>) {
--- app/src/main/java/com/example/porschehistory/screens/HistoryFragment.kt
+++ app/src/main/java/com/example/porschehistory/screens/HistoryFragment.kt
@@ -1,7 +1,6 @@
 package com.example.porschehistory.screens
 
 import android.os.Bundle
-import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,17 +10,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.porschehistory.R
 import com.example.porschehistory.data.Year
 import com.example.porschehistory.data.YearViewModel
-import com.example.porschehistory.recycler.ExampleItem
 import com.example.porschehistory.recycler.RecyclerViewAdapter
-import kotlinx.android.synthetic.main.fragment_history.*
 import kotlinx.android.synthetic.main.fragment_history.view.*
 import kotlin.random.Random
 
-class HistoryFragment : Fragment() {
+class HistoryFragment : Fragment(), RecyclerViewAdapter.OnItemClickListener {
 
     private lateinit var mYearViewModel: YearViewModel
 
@@ -34,7 +30,7 @@ class HistoryFragment : Fragment() {
 
         mYearViewModel = ViewModelProvider(this).get(YearViewModel::class.java)
 
-        val adapter = RecyclerViewAdapter()
+        val adapter = RecyclerViewAdapter(this)
         val recyclerView = view.recycler_view_history_timeline
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(
@@ -68,6 +64,14 @@ class HistoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         Log.d("CheckLog", "HistoryFragment created")
+    }
+
+    override fun onItemClick(position: Int) {
+        Toast.makeText(
+            requireContext(),
+            "You've pressed item in position $position",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
 }
